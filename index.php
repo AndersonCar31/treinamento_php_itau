fc7e9701df67afc5f20f80ea0c01c92203bc653a
AndersonCar31 <73255781+AndersonCar31@users.noreply.github.com>
2020-10-29 14:33:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php $i = (9+1); ?>
-<h1>Bem vindo turma <?php echo "$i B"; ?> com professor Anderson de Carvalho</h1>
+<h1>Bem vindo turma <?php echo "$i B"; ?> com professor Danilo Aparecido aluno Anderson de Carvalho</h1>
